caf3b548383bfd3ad936e427fa3c668dde6f36c5
Ubuntu <ubuntu@ip-10-0-0-83.ec2.internal>
2016-09-02 14:31:35 +0000

    -Sincronizacion de carpetas II

---
+++ config_test/settings.old.php
@@ -0,0 +1,19 @@
+<?php
+
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_PS_CACHING_SYSTEM_', 'CacheFs');
+define('_PS_CACHE_ENABLED_', '1');
+define('_DB_NAME_', 'farmalisto_colombia');
+define('_MYSQL_ENGINE_', 'Aria');
+define('_DB_SERVER_', '10.0.1.240');
+define('_DB_USER_', 'farmalisto');
+define('_DB_PREFIX_', 'ps_');
+define('_DB_PASSWD_', 'f4rm4l1st02013**');
+define('_COOKIE_KEY_', 'Djht58DQRt9QdPtnuFwAdrX2Rb9gAZGTVEHcY7Najayxewq8b5UeuPZK');
+define('_COOKIE_IV_', '8YnYwKpy');
+define('_PS_CREATION_DATE_', '2013-09-21');
+define('_RIJNDAEL_KEY_', '0K856YDHuizoxBhGiUQVGARePhDsoX6d');
+define('_RIJNDAEL_IV_', 'M8XjHM5Q3Ra0WgRvylO8dw==');
+define('_PS_VERSION_', '1.5.6.0');
