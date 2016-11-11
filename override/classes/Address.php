c9ea98b45306a05e96c9fd1b0c26e4a58d5e400d
desarrollo <desarrollo@CO-DESKTOP-0054>
2016-11-10 20:33:07 -0500

    cambio horario  entrega de productos

--- override/classes/Address.php
+++ override/classes/Address.php
@@ -411,7 +411,7 @@ FROM  ps_cities_col cities
         return '!';
     }
 
- public static function horaDeEntrega(){
+    public static function horaDeEntrega(){
 
     date_default_timezone_set('America/Bogota');
 
@@ -425,7 +425,7 @@ FROM  ps_cities_col cities
         $date->add(new DateInterval('PT'.$inicio_intervalos.'H'));
         $strDate = $date->format('Y-m-d H:i');
     }
-    else{ 
+    else{
         $strDate = $fecha->format('Y-m-d H:').'00'.$fecha->format(':s');
         $date = new DateTime($strDate);
         $date->add(new DateInterval('PT'.$inicio_intervalos.'H'));
@@ -475,9 +475,8 @@ FROM  ps_cities_col cities
                     $hora_in = (int)$date_1->format('H');
                     $hora_out = (int)$date_2->format('H');
 
-                   if($hora_in >= $hora_inicio && $hora_out <= $hora_fin && $hora_out > $hora_inicio)
+                    if($hora_in >= $hora_inicio && $hora_out <= $hora_fin && $hora_out > $hora_inicio)
                     {
-                         "Hora_in = ".$hora_in.", hora_inicio = ".$hora_inicio.', hora_out = '.$hora_out.', hora_fin = '.$hora_fin.'<br>';
                         $horasSelect[$date_1->format('Y-m-d')][] = ''.$date_1->format('H:i').' a '.$date_2->format('H:i').''; 
                     }
 
@@ -485,78 +484,8 @@ FROM  ps_cities_col cities
                 }
 
                 $day_delivered.= '</select>';
-                $js_json_delivered = '<script type="text/javascript"> var js_json_delivered = '.json_encode($horasSelect).'
-
-   console.log("js_json_delivered);      
-       var form_to_add = "";
-
-                $(function(){
-
-                 if( $("#form_dir").length ){
-                    form_to_add = "#form_dir"; 
-                } else{
-                    form_to_add = "#cod_form"; 
-                }              
-
-                $("<input>").attr({
-                    type: "hidden",
-                    id: "date_delivered",
-                    name: "date_delivered"
-                }).appendTo(form_to_add);
-
-$("<input>").attr({
-    type: "hidden",
-    id: "hour_delivered_h",
-    name: "hour_delivered_h"
-}).appendTo(form_to_add);
-
-$("#hour_delivered").attr("enabled", "true");
-$.each(js_json_delivered[$("#day_delivered").val()], function() {
-    $("#hour_delivered").append(
-                                $("<option></option>").text(this).val(this)
-
-                                );
-});
-
-$("#day_delivered").change(function() {
-    $("#hour_delivered").html("");              
-
-    $.each(js_json_delivered[$("#day_delivered").val()], function() {
-        $("#hour_delivered").append(
-                                    $("<option></option>").text(this).val(this)
-
-                                    );
-});
-
-$("#hour_delivered_h").val($("#hour_delivered").val());
-
-});
-
-
-$("#hour_delivered_h").val($("#hour_delivered").val());
-
-$("#date_delivered").val($("#day_delivered").val());
-
-$("#day_delivered").change(function() {
-    $("#date_delivered").val($("#day_delivered").val());
-});
-
-$("#hour_delivered").change(function() {
-    $("#hour_delivered_h").val($("#hour_delivered").val());
-});
-
-
-
-
-
-});
-
-
-
-</script>';
-return array('js_json_delivered' => $js_json_delivered, 'day_delivered' =>$day_delivered);
-echo "esto es day delivered".$day_delivered;
-} 
+      
+}
 
 
     public static function update_date_delivered(){
