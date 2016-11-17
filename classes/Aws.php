c3e2790175a4c9513259e57c86a243a304621745
andresvalenciafarmalisto <andres.valencia@farmalisto.com.co>
2016-11-16 19:44:47 -0500

    AWS S3 Inicio Imágenes Productos UP Back-Office

---
+++ classes/Aws.php
@@ -0,0 +1,100 @@
+<?php
+// Include the SDK using the Composer autoloader
+include_once(dirname(__FILE__)."/../tools/aws/aws-autoloader.php");
+use Aws\S3\MultipartUploader;
+use Aws\Exception\MultipartUploadException;
+
+/**
+ * Conexión servicio amazon web service
+ */
+class AwsCore extends ObjectModel
+{
+	public $s3Client;
+	public $bucket = 'imagenes-colombia';
+
+	/**
+	 * Datos de acceso
+	 */
+	public function __construct()
+	{
+		$this->s3Client = new Aws\S3\S3Client([
+		  'version' => 'latest',
+		  'region'  => 'us-east-1',
+		  'credentials' => [
+		        'key'    => 'AKIAIJMZFOD35IV6OCKQ',
+		        'secret' => '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',
+		    ],
+		]);
+	}
+
+	/**
+	 * Recupera objetos de Amazon S3
+	 *
+	 * @param obj string nombre de objeto
+	 * @return object 
+	 */
+	public function getObject($obj = '')
+	{
+		$result = $this->s3Client->getObject([
+		    'Bucket' => $this->bucket,
+		    'Key'    => $obj . '.jpg'
+		]);
+
+		return $result;
+	}
+
+	/**
+	 * Obtiene la URL de un objeto
+     * Este método devuelve una URL sin firmar para el bucket y key.
+     *
+     * @param obj string nombre de objeto
+     * @return string URL del objeto
+	 */
+	public function getObjectUrl($obj = '')
+	{
+		return $this->s3Client->getObjectUrl($this->bucket, $obj);
+	}
+
+	/**
+	 * Subidas de múltiples partes están diseñadas 
+	 * para mejorar la experiencia de carga de objetos más grandes
+	 * Nota: Se recomienda a los clientes de Amazon S3 
+	 * que utilicen subidas múltiples para objetos de más de 100 MB.
+	 *
+     * @see https://docs.aws.amazon.com/aws-sdk-php/v3/guide/service/s3-multipart-upload.html
+     * @param obj string ruta del objeto
+     * @return object 
+	 */
+	public function setObject($srcObj = '', $obj = '')
+	{
+		$uploader = new MultipartUploader($this->s3Client, $srcObj, [
+			'bucket' => $this->bucket,
+			'key'    => $obj,
+			'acl'    => 'public-read'  
+		]);
+
+		try {
+			return $uploader->upload();
+		} catch (MultipartUploadException $e) {
+			return $e->getMessage() . "\n";
+		}
+	}
+
+	/**
+	 * Carga un objeto de hasta 5 GB
+	 *
+     * @see http://docs.aws.amazon.com/AmazonS3/latest/dev/UploadObjSingleOpPHP.html
+     * @param obj string ruta del objeto
+     * @return object 
+	 */
+	public function setObjectImage($srcObj = '', $obj = '')
+	{
+		return $this->s3Client->putObject(array(
+			'Bucket' => $this->bucket, 
+			'Key' => $obj, 
+			'SourceFile' => $srcObj, 
+			'ACL' => 'public-read', 
+			'ContentType' => 'image/jpeg'
+		));
+	}
+}
