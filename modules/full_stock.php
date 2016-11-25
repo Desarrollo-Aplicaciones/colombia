74c30f6428ae46162da688d0822fc473f2d80622
ewing <ewing@CO-Desktop-0036>
2016-11-25 18:09:28 -0500

    Actualización de stock por error cargue y full stock

--- modules/full_stock.php
+++ modules/full_stock.php
@@ -9,7 +9,26 @@ $query_select = "SELECT COUNT(1) FROM ps_stock_available_mv";
 		print_r($results[0]);
 	}
 
+    $query_updt = ' SELECT sod.id_supply_order, COUNT(soi.id_icr) AS cant_real_rev,
+        CONCAT( "UPDATE ps_supply_order_detail sod SET sod.quantity_received =", COUNT(soi.id_icr) , " WHERE sod.id_supply_order_detail  = ", sod.id_supply_order_detail, ";" ) AS querys
+    FROM ps_supply_order_detail sod
+    LEFT JOIN ps_supply_order_icr soi on ( sod.id_supply_order_detail = soi.id_supply_order_detail )
+    WHERE sod.id_supply_order >=  14000
+    GROUP BY sod.id_supply_order_detail
+    HAVING sod.quantity_received <> COUNT(soi.id_icr)';
 
+        if ($results_upd = Db::getInstance()->Execute( $query_updt)) {
+            
+		foreach ($results_upd as $row) {
+                                        
+                        if ($results_upd_gen = Db::getInstance()->Execute( $row['querys'] )) {
+                            echo "<br> OS ".$row['id_supply_order']. " - - actulizada cant: ". $row['cant_real_rev'];
+                            
+                        }
+                    
+                }
+	}
+        
 $query_truncate = " TRUNCATE TABLE ps_stock_available_mv";
 if ($results = Db::getInstance()->Execute( $query_truncate)) {
 
