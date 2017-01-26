258141021820ee501657da7f4da3177ab477e4b2
ewing <ewing@CO-Desktop-0036>
2017-01-26 12:34:21 -0500

    Cambiar porcentaje de iva envio, ahora desde la BD tabla configuration campo IVA_ENVIO_ORDEN

--- override/classes/Cart.php
+++ override/classes/Cart.php
@@ -163,8 +163,8 @@ class Cart extends CartCore {
                 	}
                 }
             }
-
-			$valtot_tax = (round( (round($val_total*1.16)/100) *2 , 0)/ 2 ) * 100; //valor con impuesto 16% y redondeado
+                        $iva_envio_orden = Configuration::get('IVA_ENVIO_ORDEN');
+			$valtot_tax = (round( (round($val_total* ( 1 + ( $iva_envio_orden / 100 ) ) )/100) *2 , 0)/ 2 ) * 100; //valor con impuesto 16% y redondeado
 
 			if ( $val_total > $resultado[0]['delimiter2']) { // si total de compra es mayor al valor para no cobrar envio
 				$val=0;
@@ -283,7 +283,8 @@ class Cart extends CartCore {
                 	}
 			}
 			
-			$valtot_tax = (round( (round($val_total*1.16)/100) *2 , 0)/ 2 ) * 100; //valor con impuesto 16% y redondeado
+                        $iva_envio_orden = Configuration::get('IVA_ENVIO_ORDEN');
+			$valtot_tax = (round( (round($val_total* ( 1 + ($iva_envio_orden/100) ) )/100) *2 , 0)/ 2 ) * 100; //valor con impuesto 16% y redondeado
 
 			if ( $val_total > $resultado[0]['delimiter2']) { // si total de compra es mayor al valor para no cobrar envio
 				$shipping_cost=0;
