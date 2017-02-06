3afe06ede5993639f58044a521ef1418bd1a2d85
desarrollo <desarrollo@CO-DESKTOP-0029>
2017-02-06 14:17:00 -0500

    LAT-225: Estados de orden para ordenes creadas por back.

--- override/controllers/admin/AdminOrdersController.php
+++ override/controllers/admin/AdminOrdersController.php
@@ -2133,6 +2133,7 @@ return $array;
 			'langs' => Language::getLanguages(true, Context::getContext()->shop->id),
 			'payment_modules' => $payment_modules,
 			'order_states' => OrderState::getOrderStates((int)Context::getContext()->language->id,(int)$this->context->employee->id_profile),
+                        'order_states_back' => OrderState::getOrderStates( (int)Context::getContext()->language->id, (int)$this->context->employee->id_profile, true ),
 			'defaults_order_state' => $defaults_order_state,
 			'show_toolbar' => $this->show_toolbar,
 			'toolbar_btn' => $this->toolbar_btn,
