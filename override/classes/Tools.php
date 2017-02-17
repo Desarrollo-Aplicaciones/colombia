99ca0f39a614a3d8a10ae58062252e3d6a3dd743
DesarrolLo <juan.valdes@farmalisto.com.co>
2017-02-17 10:23:22 -0500

    Se realizo la validación de los clientes, no se puede crear mas de un cliente con un mismo documento de identidad.

---
+++ override/classes/Tools.php
@@ -0,0 +1,16 @@
+<?php
+class Tools extends ToolsCore {
+
+	/**
+	* Permite validar que no existan mas de dos clientes con un mismo documento de identidad.
+	**/
+	public static function validateIdentification() {
+		$sql = 'SELECT identification FROM '._DB_PREFIX_.'customer WHERE identification = %d HAVING COUNT(identification) > 1';
+		if ($total_customer = Db::getInstance()->getValue(sprintf($sql, $_POST['identification'])))
+		{
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
