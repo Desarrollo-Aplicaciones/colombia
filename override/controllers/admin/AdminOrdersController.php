902de96ed477a9dfe7ad8d64aa55496614a95ded
desarrollo <desarrollo@CO-DESKTOP-0029>
2017-03-08 14:29:31 -0500

    2- Correcion "sin stock".

--- override/controllers/admin/AdminOrdersController.php
+++ override/controllers/admin/AdminOrdersController.php
@@ -610,16 +610,20 @@ public function postProcess()
 						if (!$order->hasInvoice())
 							$use_existings_payment = true;     $this->logtxt ('linea 462 '.print_r($order,true));
 						$history->changeIdOrderState((int)$order_state->id, $order, $use_existings_payment);
+                                                error_log("\n\n Paso por aqui fofio..!! ".print_r($order_state->id,true),3,"/tmp/states.log");
                                                 
                                                 $products = $this->getProducts($order);
                                                 foreach ($products as &$product){
                                                     $product['current_stock'] = StockAvailable::getQuantityAvailableByProduct($product['product_id'], $product['product_attribute_id'], $product['id_shop']);
-                                                    if (Configuration::get('PS_STOCK_MANAGEMENT') && $product['current_stock'] < $product['product_quantity'] && $current_order_state->id == 3){
-//                                                        error_log("\n\n el producto: ".$product['product_id']." - ".$product['product_name']."No esta en stock y faltan: ".$product['out_of_stock'],3,"/tmp/states.log");
+                                                        //error_log("\n\n Paso por aqui fofio..!! ",3,"/tmp/states.log");
+                                                    
+                                                    if (Configuration::get('PS_STOCK_MANAGEMENT') && $product['current_stock'] < $product['product_quantity'] && $order_state->id == 3){
+                                                        //error_log("\n\n el producto: ".$product['product_id']." - ".$product['product_name']."No esta en stock y faltan: ".$product['out_of_stock'],3,"/tmp/states.log");
+                                                        $history->addWithemail();
                                                         $history = new OrderHistory();
                                                         $history->id_order = (int) $order->id;
                                                         $history->changeIdOrderState(Configuration::get('PS_OS_OUTOFSTOCK'), $order, true);
-                                                        $history->addWithemail();
+                                                        // $history->addWithemail();
                                                     }
                                                 }
 
